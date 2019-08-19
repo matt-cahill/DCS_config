020e96f2a693fe0c0611a9dd82488ef513668f5f
Matt Cahill <cahillenator@gmail.com>
2019-08-19 22:56:40 +1200

    switch to virtual controller for joystick

--- Config/Input/A-10C/joystick/Thrustmaster Virtual Game Controller (root) {F04B6C20-C825-11e8-8006-444553540000}.diff.lua
+++ Config/Input/A-10C/joystick/Thrustmaster Virtual Game Controller (root) {F04B6C20-C825-11e8-8006-444553540000}.diff.lua
@@ -18,15 +18,84 @@ local diff = {
 		},
 	},
 	["keyDiffs"] = {
-		["d350pnilu351cdnilvdnilvpnilvunil"] = {
-			["name"] = "Weapon release",
-			["removed"] = {
+		["d1041pnilu1042cdnilvdnilvpnilvunil"] = {
+			["added"] = {
 				[1] = {
-					["key"] = "JOY_BTN2",
+					["key"] = "JOY_BTN19",
 				},
 			},
+			["name"] = "HOTAS CMS Z Axis",
+		},
+		["d544pnilu548cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "HOTAS TMS Forward",
+		},
+		["d545pnilu548cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "HOTAS TMS Aft",
+		},
+		["d546pnilu548cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "HOTAS TMS Left",
+		},
+		["d547pnilu548cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "HOTAS TMS Right",
+		},
+		["d549pnilu553cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "HOTAS DMS Forward",
+		},
+		["d550pnilu553cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "HOTAS DMS Aft",
+		},
+		["d551pnilu553cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "HOTAS DMS Left",
+		},
+		["d552pnilu553cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "HOTAS DMS Right",
 		},
 		["d554pnilu638cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
 			["name"] = "Gun Trigger",
 			["removed"] = {
 				[1] = {
@@ -34,7 +103,60 @@ local diff = {
 				},
 			},
 		},
+		["d555pnilu639cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Gun Trigger (first stage)",
+		},
+		["d556pnilu560cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "HOTAS CMS Forward",
+		},
+		["d557pnilu560cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN17",
+				},
+			},
+			["name"] = "HOTAS CMS Aft",
+		},
+		["d558pnilu560cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN18",
+				},
+			},
+			["name"] = "HOTAS CMS Left",
+		},
+		["d559pnilu560cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+			["name"] = "HOTAS CMS Right",
+		},
+		["d561pnilu633cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "HOTAS Master Mode Control Button",
+		},
 		["d562pnilu606cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
 			["name"] = "HOTAS Nosewheel Steering Button",
 			["removed"] = {
 				[1] = {
@@ -121,6 +243,38 @@ local diff = {
 					["key"] = "JOY_BTN5",
 				},
 			},
+		},
+		["dnilp93u215cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+			["name"] = "Trim: Left Wing Down",
+		},
+		["dnilp94u215cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+			["name"] = "Trim: Right Wing Down",
+		},
+		["dnilp95u215cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+			["name"] = "Trim: Nose Down",
+		},
+		["dnilp96u215cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+			["name"] = "Trim: Nose Up",
 		},
 	},
 }
