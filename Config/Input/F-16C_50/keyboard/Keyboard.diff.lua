36a773fb820f20569c2ef7c273e8423d6fed2267
Matt Cahill <cahillenator@gmail.com>
2021-10-03 12:06:16 +1300

    update keybinds

---
+++ Config/Input/F-16C_50/keyboard/Keyboard.diff.lua
@@ -0,0 +1,17 @@
+local diff = {
+	["keyDiffs"] = {
+		["d816pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "PageDown",
+					["reformers"] = {
+						[1] = "LShift",
+						[2] = "LWin",
+					},
+				},
+			},
+			["name"] = "Active Pause",
+		},
+	},
+}
+return diff
